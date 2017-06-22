3a05ef164704c0c64e9621ce56ddf65738240982
tommy-thomas <tommyt67@gmail.com>
2017-06-22 00:08:26 -0500

    Adding .txt and .php files.

---
+++ index.maint.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * The PHP page that serves all page requests on a Drupal installation.
+ *
+ * The routines here dispatch control to the appropriate handler, which then
+ * prints the appropriate page.
+ *
+ * All Drupal code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+/**
+ * Root directory of Drupal installation.
+ */
+?>
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>The Mustard Seed Group</title>
+<body style="padding: 50px; font-size:100%; background-color:#eee;">
+<h1>We apologize, the site is currently under maintenance and should be back up shortly.</h1>
+<body>
+</html>
